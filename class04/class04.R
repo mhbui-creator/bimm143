2f8e648f7821ffa99d338eddaadfa1cb5a97548c
Melissa Bui <mhbui@ucsd.edu>
2026-05-19 20:30:09 -0700

    Added previous class folders without large PDFs

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Melissa Bui (PID:A19149673)"
+#' date: "April 9th, 2026"
+#' ---
+
+
+x <- 1:50
+plot(x,sin(x), type = "l", col="magenta", lwd=3)
+
